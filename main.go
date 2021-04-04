4a318a46876e72fab3e16904b8779a558d700786
venkyyPoojari <venkatpoojari97@gmail.com>
2021-04-05 04:04:41 +0530

    Added initial database queries

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/* Initial Database Queries
+
+Primary Student Table
+CREATE TABLE students (enrollmentNumber text, name text, class text, subject text, isDeleted boolean, PRIMARY KEY( enrollmentNumber, isDeleted));
+
+For quering with isDeleted field
+CREATE TABLE students_by_isDeleted (enrollmentNumber text, name text, class text, subject text, isDeleted boolean, PRIMARY KEY(isDeleted, enrollmentNumber));
+
+Soft deleted students
+CREATE TABLE deleted_students (enrollmentNumber text, name text, class text, subject text, PRIMARY KEY(enrollmentNumber));
+
+*/
+
 func main() {
 	models.ConnectDatabase()
 	router := gin.Default()
